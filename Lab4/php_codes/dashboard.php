8d7cbfe7a05823776fe10e29d3b1919ed6a13a55
harshahossain <72690011+harshahossain@users.noreply.github.com>
2020-11-03 16:47:19 +0600

    intially uploading in case of electrictiy

---
+++ Lab4/php_codes/dashboard.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+</head>
+<body>
+	<h1 style="background-color: black; height: 20px; width: 100%; color: white; text-align: center;">Login Approved</h1>
+</body>
+</html>
